b5e19b93d41091ee52d1cff8d5d59b6bfdf01642
rtekoh <rtekoh@outlook.com>
2024-03-26 19:12:22 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -130,7 +130,11 @@ resource "azurerm_linux_virtual_machine" "catapp" {
 
   }
 
-  tags = {}
+  tags = {
+    Name        = "${var.prefix}-hashicat-instance"
+    Environment = "prod"
+    Department  = "Hashicat Social"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
@@ -197,3 +201,10 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+
+module "azure-backup" {
+  source  = "ravensorb/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-public"
+}
+
